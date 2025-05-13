0deabec799f0b56391c1a785292be473d678dbc6
pelinkeskinn <220206006@ostimteknik.edu.tr>
2025-05-13 18:13:03 +0300

    app.py güncellendi

--- app.py
+++ app.py
@@ -1,137 +1,187 @@
 from flask import Flask, render_template, request
-from flask_socketio import SocketIO, emit, join_room
+from flask_socketio import SocketIO, emit, join_room, leave_room
 import chess
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
-# Global oyun durumu
-board = chess.Board()
-players = {}  # sid: 'white'/'black'
-assigned = {'white': None, 'black': None}
-usernames = {}  # sid -> username
-move_history = []  # hamle geçmişi
+# Global kullanıcı adı takibi
+usernames = {}        # sid -> kullanıcı adı
+sid_room  = {}        # sid -> oda adı
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# Oda bazlı durum
+rooms = {}  # room_name -> {
+            #   'board': chess.Board(),
+            #   'players': { sid: 'white'/'black', … },
+            #   'assigned': { 'white': sid or None, 'black': sid or None },
+            #   'history': [ {san, fen}, … ]
+            # }
 
-def update_player_list():
-    player_list = []
-    for sid, color in players.items():
+def init_room(room):
+    rooms[room] = {
+        'board': chess.Board(),
+        'players': {},
+        'assigned': {'white': None, 'black': None},
+        'history': []
+    }
+
+def update_player_list(room):
+    lst = []
+    for sid, color in rooms[room]['players'].items():
         name = usernames.get(sid, 'Bilinmiyor')
         renk = 'beyaz' if color == 'white' else 'siyah'
-        player_list.append({'name': name, 'color': renk})
-    socketio.emit('player_list', {'players': player_list})
+        lst.append({'name': name, 'color': renk})
+    emit('player_list', {'players': lst}, room=room)
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/game')
+def game():
+    return render_template('index.html')
 
 @socketio.on('register')
 def handle_register(data):
-    sid = request.sid
-    username = data.get('username', f'Anonim-{sid[:5]}')
-    usernames[sid] = username
-    join_room('game')  # herkes aynı odada
+    sid      = request.sid
+    username = data.get('username')
+    room     = data.get('room', 'public')
 
-    global board
-    if assigned['white'] is None and assigned['black'] is None:
-        board = chess.Board()
-        move_history.clear()
+    # İlk kez geldiyse oda yarat
+    if room not in rooms:
+        init_room(room)
 
-    if sid in players:
-        color = players[sid]
-    elif assigned['white'] is None:
-        color = 'white'
-        players[sid] = color
-        assigned['white'] = sid
-    elif assigned['black'] is None:
-        color = 'black'
-        players[sid] = color
-        assigned['black'] = sid
-    else:
-        emit('full', {'error': 'Oda dolu, izleyici moduna alındınız.'})
-        emit('watch_mode', {
-            'fen': board.fen(),
-            'turn': 'w' if board.turn == chess.WHITE else 'b',
-            'history': move_history
-        }, room=sid)
+    # Oda dolu mu?
+    if len(rooms[room]['players']) >= 2:
+        emit('room_full', {'error': 'Bu oda zaten dolu!'}, room=sid)
         return
 
+    # Kaydet
+    usernames[sid]  = username
+    sid_room[sid]   = room
+    rooms[room]['players'][sid] = None  # renk atanmamış placeholder
+
+    join_room(room)
+
+    # Renk ata
+    assigned = rooms[room]['assigned']
+    if assigned['white'] is None:
+        color = 'white'
+        assigned['white'] = sid
+    else:
+        color = 'black'
+        assigned['black'] = sid
+
+    rooms[room]['players'][sid] = color
+
+    # İlk oyuncu geldiyse tahtayı sıfırla
+    if len(rooms[room]['players']) == 1:
+        rooms[room]['board'] = chess.Board()
+        rooms[room]['history'].clear()
+
+    # Renk bilgisini gönder
     emit('color', {
         'color': color,
-        'fen': board.fen(),
-        'turn': 'w' if board.turn == chess.WHITE else 'b',
+        'fen': rooms[room]['board'].fen(),
+        'turn': 'w' if rooms[room]['board'].turn == chess.WHITE else 'b',
         'username': username,
-        'history': move_history
+        'history': rooms[room]['history']
     }, room=sid)
 
-    update_player_list()
+    # Güncel listeyi odadakilere bildir
+    update_player_list(room)
+
+@socketio.on('make_move')
+def handle_move(data):
+    sid      = request.sid
+    room     = sid_room.get(sid)
+    board    = rooms[room]['board']
+    move_san = data['move']
+
+    # Sıra kontrolü
+    player_color = rooms[room]['players'][sid]
+    if (board.turn == chess.WHITE and player_color != 'white') or \
+       (board.turn == chess.BLACK and player_color != 'black'):
+        emit('illegal_move',
+             {'error': 'Sıra sende değil!', 'fen': board.fen()},
+             room=sid)
+        return
+
+    # Hamleyi dene
+    try:
+        mv = board.parse_san(move_san)
+    except ValueError:
+        emit('illegal_move',
+             {'error': f'Geçersiz hamle: {move_san}', 'fen': board.fen()},
+             room=sid)
+        return
+
+    if mv in board.legal_moves:
+        board.push(mv)
+        rooms[room]['history'].append({'san': move_san, 'fen': board.fen()})
+
+        # Tüm odadakilere yayınla
+        emit('move_made', {
+            'move': move_san,
+            'fen': board.fen(),
+            'turn': 'w' if board.turn == chess.WHITE else 'b',
+            'history': rooms[room]['history']
+        }, room=room)
+
+        # Oyun bitti mi?
+        if board.is_game_over():
+            if board.is_checkmate():
+                loser = 'white' if board.turn == chess.WHITE else 'black'
+                winner_sid = (rooms[room]['assigned']['black']
+                              if loser == 'white'
+                              else rooms[room]['assigned']['white'])
+                winner_name = usernames.get(winner_sid, 'Bilinmiyor')
+                renk = 'beyaz' if rooms[room]['players'][winner_sid] == 'white' else 'siyah'
+                emit('game_over',
+                     {'result': f'Kazanan: {winner_name} ({renk})'},
+                     room=room)
+            else:
+                emit('game_over', {'result': board.result()}, room=room)
+    else:
+        emit('illegal_move',
+             {'error': f'Geçersiz hamle: {move_san}', 'fen': board.fen()},
+             room=sid)
+
+@socketio.on('restart_game')
+def restart_game():
+    sid  = request.sid
+    room = sid_room.get(sid)
+    rooms[room]['board']   = chess.Board()
+    rooms[room]['history'].clear()
+    emit('game_reset', {
+        'fen': rooms[room]['board'].fen(),
+        'turn': 'w',
+        'history': rooms[room]['history']
+    }, room=room)
+
+@socketio.on('time_out')
+def handle_time_out(data):
+    sid         = request.sid
+    room        = sid_room.get(sid)
+    loser_color = data.get('loserColor')
+    winner_color = 'white' if loser_color == 'black' else 'black'
+    winner_sid  = rooms[room]['assigned'][winner_color]
+
+    emit('time_out', {'loserColor': loser_color}, room=room)
 
 @socketio.on('disconnect')
 def handle_disconnect():
     sid = request.sid
-    if sid in players:
-        role = players[sid]
-        assigned[role] = None
-        del players[sid]
-    if sid in usernames:
-        del usernames[sid]
-    update_player_list()
+    room = sid_room.pop(sid, None)
+    usernames.pop(sid, None)
+    if room and sid in rooms.get(room, {}).get('players', {}):
+        rooms[room]['players'].pop(sid)
+        # assigned’ten temizle
+        for col, psid in rooms[room]['assigned'].items():
+            if psid == sid:
+                rooms[room]['assigned'][col] = None
+        leave_room(room)
+        update_player_list(room)
 
-@socketio.on('make_move')
-def handle_move(data):
-    sid = request.sid
-    move_uci = data['move']
-
-    if sid not in players:
-        emit('illegal_move', {'error': 'Oyuncu tanınmadı.', 'fen': board.fen()}, room=sid)
-        return
-
-    player_color = players[sid]
-    if (board.turn == chess.WHITE and player_color != 'white') or \
-       (board.turn == chess.BLACK and player_color != 'black'):
-        emit('illegal_move', {'error': 'Sıra sende değil!', 'fen': board.fen()}, room=sid)
-        return
-
-    legal_moves = [m.uci() for m in board.legal_moves]
-    if move_uci not in legal_moves:
-        emit('illegal_move', {'error': 'Geçersiz hamle', 'fen': board.fen()}, room=sid)
-        return
-
-    move = chess.Move.from_uci(move_uci)
-    board.push(move)
-    move_history.append({'uci': move_uci, 'fen': board.fen()})
-
-    socketio.emit('move_made', {
-        'move': move_uci,
-        'fen': board.fen(),
-        'turn': 'w' if board.turn == chess.WHITE else 'b',
-        'history': move_history
-    }, to='game')
-
-    if board.is_game_over():
-        if board.is_checkmate():
-            loser_color = 'white' if board.turn == chess.WHITE else 'black'
-            winner_sid = assigned['black'] if loser_color == 'white' else assigned['white']
-            winner_name = usernames.get(winner_sid, 'Bilinmiyor')
-            renk = 'beyaz' if players[winner_sid] == 'white' else 'siyah'
-            socketio.emit('game_over', {'result': f"Kazanan: {winner_name} ({renk})"}, to='game')
-        else:
-            socketio.emit('game_over', {'result': f"Oyun bitti: {board.result()}"}, to='game')
-
-@socketio.on('restart_game')
-def restart_game():
-    sid = request.sid
-    if sid not in players:
-        return  # sadece oyuncular oyunu başlatabilir
-
-    global board
-    board = chess.Board()
-    move_history.clear()
-
-    socketio.emit('game_reset', {
-        'fen': board.fen(),
-        'turn': 'w',
-        'history': move_history
-    }, to='game')
-
-if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True, allow_unsafe_werkzeug=True)
+    if __name__ == 'main':
+        socketio.run(app, host="0.0.0.0", port=5000, debug=True, allow_unsafe_werkzeug=True)
