a7bda0c0e4071a25226691766d6a76479b1dd0c0
pelinkeskinn <220206006@ostimteknik.edu.tr>
2025-05-13 23:20:39 +0300

    PORT ayarı Render için eklendi

--- app.py
+++ app.py
@@ -184,4 +184,6 @@ def handle_disconnect():
         update_player_list(room)
 
     if __name__ == '__main__':
-        socketio.run(app, host='0.0.0.0', port=10000)
+         import os
+         port = int(os.environ.get("PORT", 10000))
+         socketio.run(app, host='0.0.0.0', port=port)
